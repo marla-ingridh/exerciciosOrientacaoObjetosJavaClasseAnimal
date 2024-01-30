d2db6c9f7ec71fedc118cbe50ec76742ceb67d8b
Marla Ingridh <marla.ingridh@gmail.com>
2024-01-30 11:56:46 -0400

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro();
+        cachorro.emitirSom();
+        cachorro.abanarRabo();
+
+        Gato gato = new Gato();
+        gato.emitirSom();
+        gato.arranharMoveis();
+
+    }
+}
